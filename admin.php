7462ae1507ddd97c79295f54b6139bfe53a4f3c0
tabacaruandrei <andreitabacarustefan@gmail.com>
2023-06-19 09:29:03 +0300

    Fixed login system
    - fixed redirects and CSS

--- admin.php
+++ admin.php
@@ -29,7 +29,7 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
         </a>
         <nav id="header">
           <ul>
-            <li><a href="logout.php">Logout</a></li>
+            <li><a href="scripts/logout.php">Logout</a></li>
             <li><a href="contact.html">Contact</a></li>
             <li><a href="about.html">About</a></li>
           </ul>
